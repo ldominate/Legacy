46e881814324225b8e04e1206341297991cc071a
Евгений <loki_jh@bk.ru>
2016-01-20 01:07:53 +1000

    Добавил базовые классы

---
+++ Legacy.Core/Legacy.Domain/Common/BaseDomainByLogicDeleteObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Legacy.Domain.Common
+{
+	/// <summary>Базовый класс с флагом логического удаления</summary>
+	public abstract class BaseDomainByLogicDeleteObject : BaseDomainObject
+	{
+		/// <summary>Флаг логического удаления</summary>
+		public bool IsDeleted { get; set; }
+	}
+}
---
+++ Legacy.Core/Legacy.Domain/Common/BaseDomainObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Legacy.Domain.Common
+{
+	/// <summary>Базовый класс</summary>
+	public abstract class BaseDomainObject : IEquatable<BaseDomainObject>
+	{
+
+		/// <summary>Идентификатор объекта</summary>
+		public int Id { get; set; }
+
+		public bool Equals(BaseDomainObject other)
+		{
+			return other != null && other.Id == Id && other.GetType() == GetType();
+		}
+	}
+}
