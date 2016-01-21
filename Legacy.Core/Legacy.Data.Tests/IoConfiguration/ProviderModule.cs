c5dc4bf76c087387d49dcc5a86f9a8a413971d54
Евгений <loki_jh@bk.ru>
2016-01-21 13:59:08 +1000

    Добавил базовый класс с конфигурацией для тестов

---
+++ Legacy.Core/Legacy.Data.Tests/IoConfiguration/ProviderModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Legacy.Data.Tests.IoConfiguration
+{
+	public class ProviderModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+
+		}
+	}
+}
---
+++ Legacy.Core/Legacy.Data.Tests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Legacy.Data.Tests.IoConfiguration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Legacy.Data.Tests
+{
+	public abstract class TestBase
+	{
+		protected IContainer Container { get; private set; }
+
+		[TestInitialize]
+		public void Initialization()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterModule<ProviderModule>();
+
+			Container = builder.Build();
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			Container.Dispose();
+		}
+	}
+}
