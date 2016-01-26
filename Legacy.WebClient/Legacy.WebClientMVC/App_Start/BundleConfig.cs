5e6924ec65157f0c5370d47596a41514c75e9e7f
Евгений <loki_jh@bk.ru>
2016-01-27 01:33:15 +1000

    Сконфигурировал Bundle. Добавил компонент GTreeTable.

--- Legacy.WebClient/Legacy.WebClientMVC/App_Start/BundleConfig.cs
+++ Legacy.WebClient/Legacy.WebClientMVC/App_Start/BundleConfig.cs
@@ -3,6 +3,9 @@ using BundleTransformer.Core.Builders;
 using BundleTransformer.Core.Bundles;
 using BundleTransformer.Core.Orderers;
 using BundleTransformer.Core.Transformers;
+using BundleTransformer.Core.Translators;
+using BundleTransformer.Csso.Minifiers;
+using BundleTransformer.Less.Translators;
 
 namespace Legacy.WebClientMVC
 {
@@ -16,11 +19,54 @@ namespace Legacy.WebClientMVC
 			var nullOrderer = new NullOrderer();
 			var nullBuilder = new NullBuilder();
 
+			var styleTransformer = new StyleTransformer(new KryzhanovskyCssMinifier(), new ITranslator[] {new LessTranslator()})
+			{
+				CombineFilesBeforeMinification = true,
+				UsePreMinifiedFiles = true
+			};
+
+			var styleBundle = new CustomStyleBundle("~/site/style");
+			styleBundle.Include("~/content/css/site.less");
+			styleBundle.Transforms.Add(styleTransformer);
+			styleBundle.Builder = nullBuilder;
+			styleBundle.Orderer = nullOrderer;
+			bundles.Add(styleBundle);
+
+			var sBtBundle = new CustomStyleBundle("~/site/sbootstrap");
+			sBtBundle.Include("~/content/css/bootstrap/bootstrap.css",
+				"~/content/css/ie10-viewport-bug-workaround.css");
+			sBtBundle.Transforms.Add(styleTransformer);
+			sBtBundle.Builder = nullBuilder;
+			sBtBundle.Orderer = nullOrderer;
+			bundles.Add(sBtBundle);
+
+			var sGtBundle = new CustomStyleBundle("~/site/sgtreetable");
+			sGtBundle.Include("~/content/css/gtreetable/bootstrap-gtreetable.css");
+			sGtBundle.Transforms.Add(styleTransformer);
+			sGtBundle.Builder = nullBuilder;
+			sGtBundle.Orderer = nullOrderer;
+			bundles.Add(sGtBundle);
+
 			var jqBundle = new CustomScriptBundle("~/site/jq");
 			jqBundle.Include("~/content/js/jq/jquery-{version}.js");
 			jqBundle.Transforms.Add(jsTransformer);
 			jqBundle.Orderer = nullOrderer;
 			bundles.Add(jqBundle);
+
+			var jBtBundle = new CustomScriptBundle("~/site/jbootstrap");
+			jBtBundle.Include("~/content/js/bootstrap/bootstrap.js",
+				"~/content/js/ie10-viewport-bug-workaround.js");
+			jBtBundle.Transforms.Add(jsTransformer);
+			jBtBundle.Orderer = nullOrderer;
+			bundles.Add(jBtBundle);
+
+			var jGtBundle = new CustomScriptBundle("~/site/jgtreetanle");
+			jGtBundle.Include("~/content/js/gtreetable/bootstrap-gtreetable.js",
+				"~/content/js/gtreetable/languages/bootstrap-gtreetable.ru.js");
+			jGtBundle.Transforms.Add(jsTransformer);
+			jGtBundle.Orderer = nullOrderer;
+			bundles.Add(jGtBundle);
+
 		}
 	}
 }
